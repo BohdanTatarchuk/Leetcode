06e93e78d45905832ffeae88a4d5640f89a67e61
Bogdan <tatarchukbogdan80@gmail.com>
2024-01-17 23:18:36 +0100

    UniqueNumberOfOccurrences[easy]: unrequired IF was removed

--- src/main/java/org/example/Easy/UniqueNumberOfOccurrences.java
+++ src/main/java/org/example/Easy/UniqueNumberOfOccurrences.java
@@ -2,7 +2,6 @@ package org.example.Easy;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Objects;
 
 public class UniqueNumberOfOccurrences {
     public boolean uniqueOccurrences(int[] arr) {
@@ -21,17 +20,11 @@ public class UniqueNumberOfOccurrences {
         Collection<Integer> coll = map.values();
         Integer[] occurancy = coll.toArray(new Integer[coll.size()]);
 
-        if (occurancy.length == 2) {
-            if(Objects.equals(occurancy[0], occurancy[1])) {
-                result = false;
-            }
-        } else {
-            for (int j = 0; j < occurancy.length; j++) {
-                for (int a = j + 1; a < occurancy.length; a++) {
-                    if (Objects.equals(occurancy[j], occurancy[a])) {
-                        result = false;
-                        break;
-                    }
+        for (int j = 0; j < occurancy.length; j++) {
+            for (int a = j + 1; a < occurancy.length; a++) {
+                if (occurancy[j].equals(occurancy[a])) {
+                    result = false;
+                    break;
                 }
             }
         }
