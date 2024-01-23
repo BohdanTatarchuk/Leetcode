166a693509515f88464f097c919b65f6f22f44d4
Bogdan <tatarchukbogdan80@gmail.com>
2024-01-23 20:27:44 +0100

    N-aryTreePreoderTraversal[easy]: task was solved.

---
+++ src/main/java/org/example/Easy/N_aryTreePreorderTraversal.java
@@ -0,0 +1,30 @@
+package org.example.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class N_aryTreePreorderTraversal {
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if (root == null) return result;
+        result.add(root.val);
+        for (Node i : root.children) preorder(i);
+        return result;
+    }
+}
