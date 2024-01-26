d84e2f4b268f32f05ac3d9c0d8ab45173807861c
Bogdan <tatarchukbogdan80@gmail.com>
2024-01-26 22:21:39 +0100

    Maximum Depth of Binary Tree[easy]: task was solved.

--- src/main/java/org/example/Easy/UnivaluedBinaryTree.java
+++ src/main/java/org/example/Easy/UnivaluedBinaryTree.java
@@ -19,6 +19,12 @@ public class UnivaluedBinaryTree {
             this.right = right;
         }
     }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
 
     public boolean isUnivalTree(TreeNode root) {
         boolean left_correct = (root.left == null ||
